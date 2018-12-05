da9590ebb4735c24000eecdf0f94c8402fec216a
Atte Oksanen <37768995+Mixerboy24@users.noreply.github.com>
2018-12-05 15:08:26 +0200

    Create tech-bot.js

---
+++ Tech/tech-bot.js
@@ -0,0 +1,57 @@
+var Discord = require('discord.js');
+var fs = require('fs');
+var bot = new Discord.Client();
+
+bot.on('ready', () => {
+  console.log('Mixerboy24tech bot on yhdistetty serverille')
+
+  bot.user.setStatus('dnd')
+
+  bot.user.setGame('.help | Mixerboy24Tech')
+});
+
+
+bot.on('message', message => {
+
+  var sender = message.author;
+  var msg = message.content.toUpperCase();
+  var prefix = '.'
+  var cont = message.content.slice(prefix.length).split(' ');
+  var args = cont.slice(1);
+
+  if (sender.id === '517612384811220992') {
+    return;
+  }
+
+  if (msg === prefix + 'GITHUB') {
+    message.channel.send('https://github.com/Mixerboy24Tech')
+  }
+
+  if (msg === prefix + 'PING') {
+      message.channel.send('Ping ping pong. Tech bot is here')
+  }
+
+  if (msg === prefix + 'SOME') {
+      message.channel.send('Twitter: <https://www.twitter.com/Mixerboy24Tech> \n Youtube: <https://www.youtube.com/channel/UChl3Vb82N54dkX8-PHJFIhw> \n Twitch: <https://www.twitch.tv7Mixerboy24Tech> \n Facebook <https://www.facebook.com/Mixerboy24Tech>')
+  }
+
+  if (msg === prefix + 'HELP') {
+      message.channel.send('**Komennot:**\n **.some** - Mixerboy24Techin somet\n **.github** - Mixerboy24Tech organisaatio github\n **.ping**')
+  }
+
+
+  bot.on('guildMemberAdd', member => {
+    console.log('Käyttäjä ' + member.user.username + ' liityi juuri palvelimelle.')
+  
+  
+   member.guild.channels.get('513470236037480480').send('**' + member.user.username + '**, Tervetuloa Miksaaja Cityyn. Muista lukea' + message.guild.channels.find(channel => channel.name === 'tervetuloa') + ' "_confirm" komennolla saat jäsen roolin. :slight_smile: ');
+      if (send._maxListeners)
+    send.setMaxListeners(process._maxListeners + 1);
+      
+  });
+
+
+
+});
+
+bot.login('TOKEN')
